81df44ec82db78bd149c5976731698e546b3b341
Paul Wood <pmwood@pm.me>
2022-01-12 16:33:31 -0500

    Gateway can now install and register with AWS
    Issues:
    - Gateway can't communicate to PPS (/dev/spidev0.0)
        SX130x LBT not enabled
        Station device: /dev/spidev0.0 (PPS capture disabled)
        Concentrator start failed: lgw_start

---
+++ aws-register-gateway.sh
@@ -0,0 +1,42 @@
+
+# Check if aws cli is installed
+if ! command -v aws &> /dev/null; then
+    echo "The AWS CLI is required"
+    exit 1
+fi
+
+set -e
+
+[[ -z "$GATEWAY_EUI" ]] && echo "GATEWAY_EUI is required" && exit 1
+[[ -z "$GATEWAY_NAME" ]] && echo "GATEWAY_NAME is required" && exit 1
+[[ -z "$AWS_REGION" ]] && echo "AWS_REGION is required" && exit 1
+
+echo "Creating gateway"
+gateway=$(aws iotwireless create-wireless-gateway \
+  --name $GATEWAY_NAME \
+  --tags Key=application,Value=aether \
+  --lorawan GatewayEui=$GATEWAY_EUI,RfRegion="US915")
+
+echo $gateway
+gateway_id=$(echo $gateway | jq -r '.Id')
+
+echo "Getting CUPS and LNS endpoints"
+cups_endpoint=$(aws iotwireless get-service-endpoint --service-type CUPS)
+lns_endpoint=$(aws iotwireless get-service-endpoint --service-type LNS)
+lns_endpoint_id=$(echo $lns_endpoint | jq -r '.')
+
+echo $cups_endpoint | jq -r '.ServiceEndpoint' > cups.uri
+echo $cups_endpoint | jq -r '.ServerTrust' > cups.trust
+echo $lns_endpoint | jq -r '.ServiceEndpoint' > lns.uri
+echo $lns_endpoint | jq -r '.ServerTrust' > lns.trust
+
+echo "Creating certificates"
+keys_and_certs=$(aws iot create-keys-and-certificate \
+  --set-as-active \
+  --certificate-pem-outfile cups.crt \
+  --private-key-outfile cups.key)
+
+echo "Associating certificates to gateway"
+aws iotwireless associate-wireless-gateway-with-certificate \
+  --id $gateway_id \
+  --iot-certificate $(echo $keys_and_certs | jq -r '.certificateId')
--- install-basicstation.sh
+++ install-basicstation.sh
@@ -2,7 +2,7 @@
 # Followed: https://catalog.us-east-1.prod.workshops.aws/v2/workshops/b95a6659-bd4f-4567-8307-bddb43a608c4/en-US/700-advanced/dyigw-rak2245
 
 spi_file=./deps/lgw/platform-rpi/libloragw/src/loragw_spi.native.c
-temp_dir=/tmp/aether-gateway
+temp_dir=/tmp/aether
 cfg_dir=/etc/aether
 
 function cleanup {
@@ -16,27 +16,9 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-# Gateway id function from: https://github.com/RAKWireless/rak_common_for_gateway/blob/master/rak/rak/shell_script/rak_common.sh
-get_gw_id() {
-    GATEWAY_EUI_NIC="eth0"
-    if [[ `grep "$GATEWAY_EUI_NIC" /proc/net/dev` == "" ]]; then
-        GATEWAY_EUI_NIC="wlan0"
-    fi
-        if [[ `grep "$GATEWAY_EUI_NIC" /proc/net/dev` == "" ]]; then
-        GATEWAY_EUI_NIC="usb0"
-    fi
-
-    if [[ `grep "$GATEWAY_EUI_NIC" /proc/net/dev` == "" ]]; then
-       echo ""
-    fi
-    GATEWAY_EUI=$(ip link show $GATEWAY_EUI_NIC | awk '/ether/ {print $2}' | awk -F\: '{print $1$2$3"FFFE"$4$5$6}')
-    GATEWAY_EUI=${GATEWAY_EUI^^}
-    echo $GATEWAY_EUI
-}
-
 trap cleanup EXIT TERM QUIT INT
 
-chmod 755 init.sh
+rm -rf $temp_dir
 mkdir -p $temp_dir
 cd $temp_dir
 
@@ -57,16 +39,4 @@ sed -Ei 's/(#define[[:space:]]+SPI_SPEED[[:space:]]+)([[:digit:]]+)/\112000000/'
 make platform=rpi variant=std
 cp -f ./build-rpi-std/bin/station /usr/bin
 
-# Create Aether config dir, and copy files
-mkdir -p $cfg_dir
-cat ./station.json | sed "s/<GATEWAY_EUI>/$(get_gw_id)/" > $cfg_dir/station.json
-cp -f ./basicstation.service /etc/systemd/systemd
-cp -f ./init.sh $cfg_dir
-
-# Reload service spi_files 
-systemctl daemon-reload
-systemctl enable basicstation
-systemctl start basicstation
-
-echo "Finished installing Basic Station"
-ehco "Systemd 'basicstation' started"
+echo "Finished making Basic Station"
